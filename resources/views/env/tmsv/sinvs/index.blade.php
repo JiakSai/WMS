69fca7260a676f681622c8e24a87574fe28e842d
jayden <jaydenooi28@gmail.com>
2024-11-21 10:20:24 +0800

    tesrt j3

--- resources/views/env/tmsv/sinvs/index.blade.php
+++ resources/views/env/tmsv/sinvs/index.blade.php
@@ -44,7 +44,7 @@
                 
                 {{-- <th>created_by</th>
                 <th>updated_by</th> --}}
-                <th>File Nameff</th>
+                <th>File Nameffgg</th>
                 <th>Uploaded By</th>
                 <th>Uploaded On</th>
                 <th class="text-center">Action</th>
